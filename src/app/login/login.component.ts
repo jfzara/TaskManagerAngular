12cb60e60095da67f9119e5d222770d3d832c59e
jfzara <zarajeanfabrice@gmail.com>
2024-09-22 02:39:58 -0400

    Premier commit : initialisation du projet

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,36 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  isMobile: boolean = false;  // Initialisation de isMobile
 
-  constructor(private userService: UserService, private fb: FormBuilder, private authService: AuthService, private router: Router){
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
 
-  onLogin(){
-    if(this.loginForm.valid){
-      const {email, password} = this.loginForm.value;
+  ngOnInit(): void {
+    this.checkIfMobile();
+    // Ajoute un écouteur d'événements pour mettre à jour la valeur lors du redimensionnement de la fenêtre
+    window.addEventListener('resize', () => this.checkIfMobile());
+  }
+
+  checkIfMobile(): void {
+    this.isMobile = window.innerWidth <= 768 || Capacitor.isNativePlatform();
+    if (this.isMobile) {
+      document.body.classList.add('mobile');
+    } else {
+      document.body.classList.remove('mobile');
+    }
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
       this.userService.login(email, password).subscribe(
         (res) => {
           console.log(res);
-          this.authService.setToken(res.token, res.loggedUser)
-          this.router.navigate(["/"])
+          this.authService.setToken(res.token, res.loggedUser);
+          this.router.navigate(["/"]);
         },
         (err) => {
-          console.log(err)
+          console.log(err);
         }
       );
     }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
@@ -7,12 +7,18 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
   signupForm: FormGroup;
+  isMobile: boolean = false;
 
-  constructor(private userService: UserService, private fb: FormBuilder, private authService: AuthService, private router: Router){
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.signupForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -20,16 +26,26 @@ export class SignupComponent {
     });
   }
 
-  onSignUp(){
-    if(this.signupForm.valid){
-      const {name, email, password} = this.signupForm.value;
+  ngOnInit(): void {
+    this.checkIfMobile();
+   
+  }
+
+  @HostListener('window:resize', ['$event'])
+  checkIfMobile(): void {
+    this.isMobile = window.innerWidth <= 768;
+  }
+
+  onSignUp(): void {
+    if (this.signupForm.valid) {
+      const { name, email, password } = this.signupForm.value;
       this.userService.signup(email, password, name).subscribe(
         (res) => {
           console.log(res);
-          this.router.navigate(["/login"])
+          this.router.navigate(['/login']);
         },
         (err) => {
-          console.log(err)
+          console.log(err);
         }
       );
     }
